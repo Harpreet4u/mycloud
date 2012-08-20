ee3b4f07f2c787c97f0b61f9ceb0c5a3c55dbdc1
Ankit Bhatnagar <ankitbhatnagar2012@gmail.com>
2012-08-20 23:42:14 +0530

    Initial commit & Login UI

---
+++ scratch/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+  <title>MyCloud - Login</title>
+  <meta name="description" content="" />
+  <meta name="author" content="Ankit" />
+  <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+  <!-- CSS styling -->
+  <link rel="stylesheet" href="./css/login_page.css" />
+</head>
+
+<body>
+  <div>
+  	<!-- Header content -->
+    <header></header>
+    
+    <!-- Navigation content -->
+    <nav></nav>
+
+    <!-- Main content -->
+    <div id="container">
+    	<div id="left_content">
+    		<center>
+    			<img src="./images/project_logo.png" alt="Project Logo" height="400px" /><br />
+    			<div style="font-family: Palatino Linotype; font-size: 70px; font-weight: bold;">&lt;&nbsp;MyCloud&nbsp;/&gt;</div><br />
+    			<div style="font-family: Palatino Linotype; font-size: 30px; font-weight: bold;">Your Desktop, Wherever You Go.</div><br />
+    		</center>
+    	</div>
+    	<div id="right_content">
+    		<div style="font-family: Palatino Linotype; font-size: 20px; font-weight: bold; padding-left: 20px;">Login -</div><br />
+    		<center>
+    			<a href="#"><img src="./images/google.png" /></a>
+    			<a href="#"><img src="./images/facebook.png" /></a>
+    			<a href="#"><img src="./images/twitter.png" /></a>
+    		</center>    			
+    	</div>
+    	<div id="bottom_content">
+    		<div style="font-family: Palatino Linotype; font-size: 10px; font-weight: bold; padding-left: 80%; ">
+    			 <a href="#">ABOUT US</a>&nbsp;|&nbsp;
+    			 <a href="#">CONTACT US</a>&nbsp;|&nbsp;
+    			 <a href="#">DEVELOPERS</a>
+    		</div>
+    		
+    	</div>
+    </div>
+
+    <!-- Footer content -->
+    <footer></footer>
+  </div>
+</body>
+</html>
